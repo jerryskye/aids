8310dc635f0544e09eaa9bc40939edab51c1f6a4
jerryskye <jarek@siedlarz.com>
2016-12-09 13:31:34 +0100

    quick fixes + postscript

--- binary_trees/binary_tree.h
+++ binary_trees/binary_tree.h
@@ -8,12 +8,18 @@ bool search(node* tree, int x) {
 	return tree? (tree->data == x || search(tree->left, x) || search(tree->right, x)) : false;
 }
 
-int leaves(node* tree) {
-	return tree? (leaves(tree->right) + leaves(tree->left) + 1) : 0;
+int vertexes(node* tree) { //ilość wierzchołków
+	return tree? (vertexes(tree->right) + vertexes(tree->left) + 1) : 0;
+}
+
+int leaves(node* tree) { //ilość liści
+	//return tree? (leaves(tree->right) + leaves(tree->left)) : 0;
+	//do poprawienia, ktoś kto ma niech napisze, moje jest źle
+	return 0xdeadbeef;
 }
 
 int height(node* tree) {
-	return tree? 1 + max(height(tree->left), height(tree->right)) : -1;
+	return tree? 1 + std::max(height(tree->left), height(tree->right)) : -1;
 }
 
 int right_children(node* tree) {
@@ -28,9 +34,8 @@ int right_children(node* tree) {
 
 void print(node* tree, int p = 0) {
 	if(tree) {
-		for(int i = 0; i < p; ++i)
-			cout << " ";
-		cout << tree->data << endl;
+		std::cout << std::string(p, ' '); //wypisz p spacji - specjalny konstruktor string(int, char)
+		std::cout << tree->data << std::endl;
 		print(tree->left, p + 1);
 		print(tree->right, p + 1);
 	}
@@ -39,8 +44,10 @@ void print(node* tree, int p = 0) {
 bool balanced(node* tree) {
 	if(tree == 0)
 		return true;
-	switch(heigth(tree->left) - height(tree->right)) {
-		case -1, 0, 1:
+	switch(height(tree->left) - height(tree->right)) {
+		case -1:
+		case 0:
+		case 1:
 			return balanced(tree->left) && balanced(tree->right);
 		default:
 			return false;
